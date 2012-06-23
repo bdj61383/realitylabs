82f24685bdf0b199bfdb146fb677d0c9e5262e8b
bdj61383 <brian.leroy.johnson@gmail.com>
2012-06-23 14:52:03 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Realitylabs::Application.config.secret_token = '835819771e0c8b90274de4e2de62f44ab12963cd8d107d907bbd9a2111fc6e83552406ad69d1c19766ca53222b783dced052b50068b70277461e331452a09535'
