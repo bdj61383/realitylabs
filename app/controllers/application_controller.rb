e253748c3386ae2b73096a958088c3c3eedc3636
bdj61383 <brian.leroy.johnson@gmail.com>
2012-08-07 09:44:17 -0700

    Still configuring faye for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,11 +17,13 @@ class ApplicationController < ActionController::Base
   # text/plain.
 
   def cors_preflight_check
+    if request.method == :options
       headers['Access-Control-Allow-Origin'] = '*'
       headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = 'X-Requested-With'
+      headers['Access-Control-Allow-Headers'] = '*'
       headers['Access-Control-Max-Age'] = '1728000'
       render :text => '', :content_type => 'text/plain'
+    end
   end
 
   private
