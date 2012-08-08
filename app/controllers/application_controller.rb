2b70c1eb16af3c7575b8fb3610b1dc12153b772b
bdj61383 <brian.leroy.johnson@gmail.com>
2012-08-08 09:40:34 -0700

    Rewrote draft_page so that faye works correctly.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :update_last_seen
-  before_filter :cors_preflight_check
-  after_filter :cors_set_access_control_headers
+  # before_filter :cors_preflight_check
+  # after_filter :cors_set_access_control_headers
 
-  # For all responses in this controller, return the CORS access control headers.
+  # # For all responses in this controller, return the CORS access control headers.
 
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Max-Age'] = "1728000"
-  end
+  # def cors_set_access_control_headers
+  #   headers['Access-Control-Allow-Origin'] = '*'
+  #   headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+  #   headers['Access-Control-Max-Age'] = "1728000"
+  # end
 
-  # If this is a preflight OPTIONS request, then short-circuit the
-  # request, return only the necessary headers and return an empty
-  # text/plain.
+  # # If this is a preflight OPTIONS request, then short-circuit the
+  # # request, return only the necessary headers and return an empty
+  # # text/plain.
 
-  def cors_preflight_check
-    if request.method == :options
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = '*'
-      headers['Access-Control-Max-Age'] = '1728000'
-      render :text => '', :content_type => 'text/plain'
-    end
-  end
+  # def cors_preflight_check
+  #   if request.method == :options
+  #     headers['Access-Control-Allow-Origin'] = '*'
+  #     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+  #     headers['Access-Control-Allow-Headers'] = '*'
+  #     headers['Access-Control-Max-Age'] = '1728000'
+  #     render :text => '', :content_type => 'text/plain'
+  #   end
+  # end
 
   def broadcast_server
       if request.port.to_i != 80
