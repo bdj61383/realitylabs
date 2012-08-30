e4a33b0cd29e29b32edc21bac304619eb0795993
bdj61383 <brian.leroy.johnson@gmail.com>
2012-08-29 19:13:47 -0700

    Changed Contestant#rake_db_copy to work with ruby v 1.9.2, as used by Heroku.  It was written using ruby 1.8.7 that came with the mac

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -9,12 +9,12 @@ class Contestant < ActiveRecord::Base
 	    # Looks to see if the database copy already exists.  If so, it deletes it and puts the new copy in.
 	    @round = Contestant.first.round
 	    @time = Time.now.to_s.gsub!(/ /, '_')
-	    @dir = Dir.entries("db").select {|x| x =~ /^round#{@round}/}.to_s
-	    	# if @dir != nil
-	    	if @dir.length != 0
-	    		@dir.each do |directory|
-				system("rm -rf db/#{directory.to_s}")
-				end
+	    @dir = Dir.entries("db").select {|x| x =~ /^round#{@round}/}.join
+	    	if @dir != ""
+	    		system("rm -rf db/#{@dir}")
+	    		# @dir.each do |directory|
+				# system("rm -rf db/#{directory.to_s}")
+				# end
 			end
 	    system("rake db:data:dump_dir dir=round#{@round}_#{@time}") #Apparently this isn't the most efficient way to do this, as it loads a new instance of rails or something like that.  It is working for the time being.
 	    # system("rake db:data:dump_dir dir=round#{@round}")
@@ -22,7 +22,7 @@ class Contestant < ActiveRecord::Base
 
   	def self.rake_db_load # This reloads the results of the previous round.
   		@last_round = Contestant.first.round - 1
-  		@dir = Dir.entries("db").select{|x| x =~ /^round#{@last_round}/}.to_s
+  		@dir = Dir.entries("db").select{|x| x =~ /^round#{@last_round}/}.join
   		system("rake db:data:load_dir dir=#{@dir}")
   	end
 end
