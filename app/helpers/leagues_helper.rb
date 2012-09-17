12c30a456ca8262364891601711b23fb3dcb07be
bdj61383 <brian.leroy.johnson@gmail.com>
2012-09-17 12:15:46 -0700

    Reverted contestant seeds to state prior to adding bio info.  Bio info is now added on the DOM level by javascript, due to problems with formatting in postgres.

--- app/helpers/leagues_helper.rb
+++ app/helpers/leagues_helper.rb
@@ -158,11 +158,65 @@ module LeaguesHelper
 								haml_concat image_tag filename
 								haml_tag :h4, member
 							end
+							haml_tag :div, :class => 'cast_bio_draft', :id => (member.gsub(" ", "_") + "_bio") do
+							# contestant = Contestant.find_by_name(member)
+							# haml_tag :p do
+							# 	haml_tag :strong, contestant.nickname
+							# 	haml_tag :br
+							# 	haml_tag :b, "Tribe"
+							# 	haml_tag :span, contestant.tribe
+							# 	haml_tag :br
+							# 	haml_tag :b, "Age"
+							# 	haml_tag :span, contestant.age
+							# 	haml_tag :br
+							# 	haml_tag :b, "Hometown"
+							# 	haml_tag :span, contestant.hometown
+							# 	haml_tag :br
+							# 	haml_tag :b, "Occupation"
+							# 	haml_tag :span, contestant.occupation
+							# 	haml_tag :br
+							# 	haml_tag :b, "Reality Labs Pre-Season Rank"
+							# 	haml_tag :span, contestant.preseason_rank
+							# 	haml_tag :br
+							# 	haml_tag :b, "The Lab Work"
+							# 	haml_tag :span, :class => 'bio_float' do 
+							# 		haml_concat contestant.labwork
+							# 	end
+							# 	haml_tag :br
+							# end
+							end
 						else
 							haml_tag :button, :id => member.gsub(" ", "_"), :class => 'unavailable' do
 								filename = "contestants/" + member.gsub(" ", "_") + ".jpg"
 								haml_concat image_tag filename
 								haml_tag :h4, member
+							end
+							haml_tag :div, :class => 'cast_bio_draft', :id => (member.gsub(" ", "_") + "_bio") do
+							# contestant = Contestant.find_by_name(member)
+							# haml_tag :p do
+							# 	haml_tag :strong, contestant.nickname
+							# 	haml_tag :br
+							# 	haml_tag :b, "Tribe"
+							# 	haml_tag :span, contestant.tribe
+							# 	haml_tag :br
+							# 	haml_tag :b, "Age"
+							# 	haml_tag :span, contestant.age
+							# 	haml_tag :br
+							# 	haml_tag :b, "Hometown"
+							# 	haml_tag :span, contestant.hometown
+							# 	haml_tag :br
+							# 	haml_tag :b, "Occupation"
+							# 	haml_tag :span, contestant.occupation
+							# 	haml_tag :br
+							# 	haml_tag :b, "Reality Labs Pre-Season Rank"
+							# 	haml_tag :span, contestant.preseason_rank
+							# 	haml_tag :br
+							# 	haml_tag :b, "The Lab Work"
+							# 	haml_tag :span, :class => 'bio_float' do 
+							# 		haml_concat contestant.labwork
+							# 	end
+							# 	haml_tag :br
+							# end
 							end
 						end
 					end
@@ -174,11 +228,65 @@ module LeaguesHelper
 								haml_concat image_tag filename
 								haml_tag :h4, member
 							end
+							haml_tag :div, :class => 'cast_bio_draft', :id => (member.gsub(" ", "_") + "_bio") do
+							# contestant = Contestant.find_by_name(member)
+							# haml_tag :p do
+							# 	haml_tag :strong, contestant.nickname
+							# 	haml_tag :br
+							# 	haml_tag :b, "Tribe"
+							# 	haml_tag :span, contestant.tribe
+							# 	haml_tag :br
+							# 	haml_tag :b, "Age"
+							# 	haml_tag :span, contestant.age
+							# 	haml_tag :br
+							# 	haml_tag :b, "Hometown"
+							# 	haml_tag :span, contestant.hometown
+							# 	haml_tag :br
+							# 	haml_tag :b, "Occupation"
+							# 	haml_tag :span, contestant.occupation
+							# 	haml_tag :br
+							# 	haml_tag :b, "Reality Labs Pre-Season Rank"
+							# 	haml_tag :span, contestant.preseason_rank
+							# 	haml_tag :br
+							# 	haml_tag :b, "The Lab Work"
+							# 	haml_tag :span, :class => 'bio_float' do 
+							# 		haml_concat contestant.labwork
+							# 	end
+							# 	haml_tag :br
+							# end
+							end
 						else
 							haml_tag :button, :id => member.gsub(" ", "_"), :class => 'unavailable' do
 								filename = "contestants/" + member.gsub(" ", "_") + ".jpg"
 								haml_concat image_tag filename
 								haml_tag :h4, member
+							end
+							haml_tag :div, :class => 'cast_bio_draft', :id => (member.gsub(" ", "_") + "_bio") do
+							# contestant = Contestant.find_by_name(member)
+							# haml_tag :p do
+							# 	haml_tag :strong, contestant.nickname
+							# 	haml_tag :br
+							# 	haml_tag :b, "Tribe"
+							# 	haml_tag :span, contestant.tribe
+							# 	haml_tag :br
+							# 	haml_tag :b, "Age"
+							# 	haml_tag :span, contestant.age
+							# 	haml_tag :br
+							# 	haml_tag :b, "Hometown"
+							# 	haml_tag :span, contestant.hometown
+							# 	haml_tag :br
+							# 	haml_tag :b, "Occupation"
+							# 	haml_tag :span, contestant.occupation
+							# 	haml_tag :br
+							# 	haml_tag :b, "Reality Labs Pre-Season Rank"
+							# 	haml_tag :span, contestant.preseason_rank
+							# 	haml_tag :br
+							# 	haml_tag :b, "The Lab Work"
+							# 	haml_tag :span, :class => 'bio_float' do 
+							# 		haml_concat contestant.labwork
+							# 	end
+							# 	haml_tag :br
+							# end
 							end
 						end
 					end
@@ -190,11 +298,65 @@ module LeaguesHelper
 								haml_concat image_tag filename
 								haml_tag :h4, member
 							end
+							haml_tag :div, :class => 'cast_bio_draft', :id => (member.gsub(" ", "_") + "_bio") do
+							# contestant = Contestant.find_by_name(member)
+							# haml_tag :p do
+							# 	haml_tag :strong, contestant.nickname
+							# 	haml_tag :br
+							# 	haml_tag :b, "Tribe"
+							# 	haml_tag :span, contestant.tribe
+							# 	haml_tag :br
+							# 	haml_tag :b, "Age"
+							# 	haml_tag :span, contestant.age
+							# 	haml_tag :br
+							# 	haml_tag :b, "Hometown"
+							# 	haml_tag :span, contestant.hometown
+							# 	haml_tag :br
+							# 	haml_tag :b, "Occupation"
+							# 	haml_tag :span, contestant.occupation
+							# 	haml_tag :br
+							# 	haml_tag :b, "Reality Labs Pre-Season Rank"
+							# 	haml_tag :span, contestant.preseason_rank
+							# 	haml_tag :br
+							# 	haml_tag :b, "The Lab Work"
+							# 	haml_tag :span, :class => 'bio_float' do 
+							# 		haml_concat contestant.labwork
+							# 	end
+							# 	haml_tag :br
+							# end
+							end
 						else
 							haml_tag :button, :id => member.gsub(" ", "_"), :class => 'unavailable' do
 								filename = "contestants/" + member.gsub(" ", "_") + ".jpg"
 								haml_concat image_tag filename
 								haml_tag :h4, member
+							end
+							haml_tag :div, :class => 'cast_bio_draft', :id => (member.gsub(" ", "_") + "_bio") do
+							# contestant = Contestant.find_by_name(member)
+							# haml_tag :p do
+							# 	haml_tag :strong, contestant.nickname
+							# 	haml_tag :br
+							# 	haml_tag :b, "Tribe"
+							# 	haml_tag :span, contestant.tribe
+							# 	haml_tag :br
+							# 	haml_tag :b, "Age"
+							# 	haml_tag :span, contestant.age
+							# 	haml_tag :br
+							# 	haml_tag :b, "Hometown"
+							# 	haml_tag :span, contestant.hometown
+							# 	haml_tag :br
+							# 	haml_tag :b, "Occupation"
+							# 	haml_tag :span, contestant.occupation
+							# 	haml_tag :br
+							# 	haml_tag :b, "Reality Labs Pre-Season Rank"
+							# 	haml_tag :span, contestant.preseason_rank
+							# 	haml_tag :br
+							# 	haml_tag :b, "The Lab Work"
+							# 	haml_tag :span, :class => 'bio_float' do 
+							# 		haml_concat contestant.labwork
+							# 	end
+							# 	haml_tag :br
+							# end
 							end
 						end
 					end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -10,31 +10,31 @@ module WelcomeHelper
 							haml_tag :h4, member
 						end
 						haml_tag :div, :class => 'cast_bio', :id => (member.gsub(" ", "_") + "_bio") do
-							contestant = Contestant.find_by_name(member)
-							haml_tag :p do
-								haml_tag :strong, contestant.nickname
-								haml_tag :br
-								haml_tag :b, "Tribe"
-								haml_tag :span, contestant.tribe
-								haml_tag :br
-								haml_tag :b, "Age"
-								haml_tag :span, contestant.age
-								haml_tag :br
-								haml_tag :b, "Hometown"
-								haml_tag :span, contestant.hometown
-								haml_tag :br
-								haml_tag :b, "Occupation"
-								haml_tag :span, contestant.occupation
-								haml_tag :br
-								haml_tag :b, "Reality Labs Pre-Season Rank"
-								haml_tag :span, contestant.preseason_rank
-								haml_tag :br
-								haml_tag :b, "The Lab Work"
-								haml_tag :span, :class => 'bio_float' do 
-									haml_concat contestant.labwork
-								end
-								haml_tag :br
-							end
+							# contestant = Contestant.find_by_name(member)
+							# haml_tag :p do
+							# 	haml_tag :strong, contestant.nickname
+							# 	haml_tag :br
+							# 	haml_tag :b, "Tribe"
+							# 	haml_tag :span, contestant.tribe
+							# 	haml_tag :br
+							# 	haml_tag :b, "Age"
+							# 	haml_tag :span, contestant.age
+							# 	haml_tag :br
+							# 	haml_tag :b, "Hometown"
+							# 	haml_tag :span, contestant.hometown
+							# 	haml_tag :br
+							# 	haml_tag :b, "Occupation"
+							# 	haml_tag :span, contestant.occupation
+							# 	haml_tag :br
+							# 	haml_tag :b, "Reality Labs Pre-Season Rank"
+							# 	haml_tag :span, contestant.preseason_rank
+							# 	haml_tag :br
+							# 	haml_tag :b, "The Lab Work"
+							# 	haml_tag :span, :class => 'bio_float' do 
+							# 		haml_concat contestant.labwork
+							# 	end
+							# 	haml_tag :br
+							# end
 						end
 					end
 				elsif (@contestants.index(member) % 3) == 1
@@ -45,31 +45,31 @@ module WelcomeHelper
 							haml_tag :h4, member
 						end
 						haml_tag :div, :class => 'cast_bio', :id => (member.gsub(" ", "_") + "_bio") do
-							contestant = Contestant.find_by_name(member)
-							haml_tag :p do
-								haml_tag :strong, contestant.nickname
-								haml_tag :br
-								haml_tag :b, "Tribe"
-								haml_tag :span, contestant.tribe
-								haml_tag :br
-								haml_tag :b, "Age"
-								haml_tag :span, contestant.age
-								haml_tag :br
-								haml_tag :b, "Hometown"
-								haml_tag :span, contestant.hometown
-								haml_tag :br
-								haml_tag :b, "Occupation"
-								haml_tag :span, contestant.occupation
-								haml_tag :br
-								haml_tag :b, "Reality Labs Pre-Season Rank"
-								haml_tag :span, contestant.preseason_rank
-								haml_tag :br
-								haml_tag :b, "The Lab Work"
-								haml_tag :span, :class => 'bio_float' do 
-									haml_concat contestant.labwork
-								end
-								haml_tag :br
-							end
+							# contestant = Contestant.find_by_name(member)
+							# haml_tag :p do
+							# 	haml_tag :strong, contestant.nickname
+							# 	haml_tag :br
+							# 	haml_tag :b, "Tribe"
+							# 	haml_tag :span, contestant.tribe
+							# 	haml_tag :br
+							# 	haml_tag :b, "Age"
+							# 	haml_tag :span, contestant.age
+							# 	haml_tag :br
+							# 	haml_tag :b, "Hometown"
+							# 	haml_tag :span, contestant.hometown
+							# 	haml_tag :br
+							# 	haml_tag :b, "Occupation"
+							# 	haml_tag :span, contestant.occupation
+							# 	haml_tag :br
+							# 	haml_tag :b, "Reality Labs Pre-Season Rank"
+							# 	haml_tag :span, contestant.preseason_rank
+							# 	haml_tag :br
+							# 	haml_tag :b, "The Lab Work"
+							# 	haml_tag :span, :class => 'bio_float' do 
+							# 		haml_concat contestant.labwork
+							# 	end
+							# 	haml_tag :br
+							# end
 						end
 					end
 				else
@@ -80,31 +80,31 @@ module WelcomeHelper
 							haml_tag :h4, member
 						end
 						haml_tag :div, :class => 'cast_bio', :id => (member.gsub(" ", "_") + "_bio") do
-							contestant = Contestant.find_by_name(member)
-							haml_tag :p do
-								haml_tag :strong, contestant.nickname
-								haml_tag :br
-								haml_tag :b, "Tribe"
-								haml_tag :span, contestant.tribe
-								haml_tag :br
-								haml_tag :b, "Age"
-								haml_tag :span, contestant.age
-								haml_tag :br
-								haml_tag :b, "Hometown"
-								haml_tag :span, contestant.hometown
-								haml_tag :br
-								haml_tag :b, "Occupation"
-								haml_tag :span, contestant.occupation
-								haml_tag :br
-								haml_tag :b, "Reality Labs Pre-Season Rank"
-								haml_tag :span, contestant.preseason_rank
-								haml_tag :br
-								haml_tag :b, "The Lab Work"
-								haml_tag :span, :class => 'bio_float' do 
-									haml_concat contestant.labwork
-								end
-								haml_tag :br
-							end
+							# contestant = Contestant.find_by_name(member)
+							# haml_tag :p do
+							# 	haml_tag :strong, contestant.nickname
+							# 	haml_tag :br
+							# 	haml_tag :b, "Tribe"
+							# 	haml_tag :span, contestant.tribe
+							# 	haml_tag :br
+							# 	haml_tag :b, "Age"
+							# 	haml_tag :span, contestant.age
+							# 	haml_tag :br
+							# 	haml_tag :b, "Hometown"
+							# 	haml_tag :span, contestant.hometown
+							# 	haml_tag :br
+							# 	haml_tag :b, "Occupation"
+							# 	haml_tag :span, contestant.occupation
+							# 	haml_tag :br
+							# 	haml_tag :b, "Reality Labs Pre-Season Rank"
+							# 	haml_tag :span, contestant.preseason_rank
+							# 	haml_tag :br
+							# 	haml_tag :b, "The Lab Work"
+							# 	haml_tag :span, :class => 'bio_float' do 
+							# 		haml_concat contestant.labwork
+							# 	end
+							# 	haml_tag :br
+							# end
 						end
 					end
 				end
